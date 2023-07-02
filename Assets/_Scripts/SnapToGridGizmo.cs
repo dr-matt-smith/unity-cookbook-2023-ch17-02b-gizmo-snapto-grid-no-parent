e58585e2701696796c97c92e43812d6d8acbb9fa
dr-matt-smith <matt.smith@tudublin.ie>
2023-07-02 11:28:40 +0100

    added files to project

---
+++ Assets/_Scripts/SnapToGridGizmo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapToGridGizmo : MonoBehaviour
+{
+    /**
+     * we've moved!
+     * snap position to its grid
+     */
+    public void Update()
+    {
+#if UNITY_EDITOR
+        GridGizmo.GetInstance().SnapPositionToGrid(transform);
+#endif
+    }
+    
+}
